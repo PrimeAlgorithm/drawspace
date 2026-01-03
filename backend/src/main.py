779b549413f68b0e7f3aeed9c9750e93ea6d8eb0
PrimeAlgorithm <219721209+PrimeAlgorithm@users.noreply.github.com>
2026-01-02 16:52:34 -0800

    Connect auth routing

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.database import Base, engine
 from src import models
+from src.api.v1 import api_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,7 +12,7 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
-
+app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
